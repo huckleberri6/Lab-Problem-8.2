60c511eb21100b6e14059fca3c9fb66b5d066321
huckleberri6 <j-devore.1@onu.edu>
2023-10-19 11:24:17 -0400

    completed countWords function

--- Lab Problem 8.2/solution.cpp
+++ Lab Problem 8.2/solution.cpp
@@ -1,1 +1,44 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countWords(string str);
+
+int main()
+{
+	string s = "The quick brown fox jumped over the lazy dog";
+	int numWords = countWords(s);
+	cout << numWords;
+}
+
+int countWords(string str)
+{
+	int words = 0;
+	if (str == "")
+		return words;
+	else
+		words++;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		bool space = false;
+		bool firstLetter = false;
+		if (str.substr(i, 1) == " ")
+			space = true;
+		while (space == true)
+		{
+			i++;
+			if (str.substr(i, 1) == " ")
+				space = true;
+			else
+			{
+				firstLetter = true;
+				break;
+			}
+		}
+		if (firstLetter)
+			words++;
+	}
+	return words;
+}
